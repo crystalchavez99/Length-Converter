6d4c1fa6ebaeee338f87fed1861b0c3ec619de4e
Crystal Chavez <88100670+crystalchavez99@users.noreply.github.com>
2022-02-16 20:06:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function convert() {
-    let inputValue = document.getElementById('firstValue').value;
-    console.log(inputValue);
-    let result = document.getElementById('secondValue');
-    result.value = inputValue / 3.2808;
-  }
+//function convert() {
+//    let inputValue = document.getElementById('firstValue').value;
+//    console.log(inputValue);
+//    let result = document.getElementById('secondValue');
+//    result.value = inputValue / 3.2808;
+//  }
